78362f5e45676520808edd52d9be6a416ba79210
Sam Ahrens <339729139@GoTVDSB.ca>
2024-09-06 09:39:56 -0400

    Add project files.

---
+++ Topic 2 - Numbers and Math/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Topic_2___Numbers_and_Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I will now count my chickens:");
+            Console.WriteLine("Hens " + (10 + 5 * 2));
+            Console.WriteLine("Roosters " + (25 + 30 / 6));
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("I will now count the eggs:");
+            Console.WriteLine(11.0 / 2);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Is it true that 3 + 2 < 5 - 7?");
+            Console.WriteLine(3 + 2 < 5 - 7);
+            Console.WriteLine("What is 3 + 2?" + (3 + 2));
+            Console.WriteLine("What is 5 - 7?" + (5 - 7));
+            Console.WriteLine("Oh that's why it's " + (3 + 2 < 5 - 7));
+        } 
+    }
+}
